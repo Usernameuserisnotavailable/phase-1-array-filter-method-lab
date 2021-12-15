81302e5e9586ac9a266a755b82883344c2ce0a72
Jose Garcia <rocketrito1@gmail.com>
2021-12-15 14:43:53 -0800

    commit tree 38193babc1516cc0270dae2e6652d0377c391e50

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const findMatching = (driverArray, driver) => {
+    return driverArray.filter(human => {
+        return human.toLowerCase() === driver.toLowerCase()
+        })
+    }
+    
+    const fuzzyMatch = (driverArray, firstLetters) => {
+        return driverArray.filter(driver => {
+            const testLength = firstLetters.length
+            return driver.slice(0, testLength).toLowerCase() === firstLetters.toLowerCase()
+    
+        })
+    }
+    const matchName = (driverArray, driver) => {
+        return driverArray.filter(driverObj => {
+           return driverObj.name === driver
+        })
+    } 
+    
